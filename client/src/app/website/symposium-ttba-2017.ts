70bf0aa483636d5691236c19762d1b0e97699672
yuntichen <ychen164@mgh.harvard.edu>
2026-05-26 08:30:44 -0400

    Update symposium-ttba-2017.ts

--- client/src/app/website/symposium-ttba-2017.ts
+++ client/src/app/website/symposium-ttba-2017.ts
@@ -113,7 +113,13 @@ export const ttbaSymposium2017 = {
 			'to': new Date('2026/07/11 13:30 EDT'),
 			'title': 'Lunch with Roundtable',
 			'topic': null,
-			'sessionDescription': '',
+			'sessionDescription': '1. Job hunting and interview\
+									2. Resume Tune-Up Clinic\
+									3. From Uncertainty to Opportunity: The Mindset for Career Transition\
+									4. Build Your First AI Agent Workflow\
+									5. Bridge the Gap: Communication Skills in Academia and Industry\
+									6. CMC\
+									7. Taiwan Academia: Current Landscape and Future Directions',
 			'tags': [],
 			'rooms': [],
 			'speakers': []
